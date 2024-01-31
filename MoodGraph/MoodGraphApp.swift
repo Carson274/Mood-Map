a6b9dcc4e6d299d4a809c7985ed49497007a8aaf
Carson274 <carsonsecrest@gmail.com>
2024-01-30 20:41:26 -0800

    Initial Commit

---
+++ MoodGraph/MoodGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodGraphApp.swift
+//  MoodGraph
+//
+//  Created by Carson Secrest on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
