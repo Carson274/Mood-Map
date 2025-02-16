c68fdc46bab66286bda74baca883f7bc485df8d9
Carson Secrest <carsonsecrest@gmail.com>
2025-02-16 03:51:10 -0800

    Create functions to retrieve and create quotes

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -14,6 +14,8 @@ const db = getFirestore("mood-map");
 // npm --prefix ./functions run lint -- --fix
 // firebase deploy --only functions functions:addMessage,functions:makeUppercase
 
+
+//////////////////////////////////////// ADD MOOD ////////////////////////////////////////
 // Add mood function using onRequest
 exports.addMood = onRequest(async (req, res) => {
   // Log the start of the function
@@ -69,6 +71,8 @@ exports.addMood = onRequest(async (req, res) => {
   }
 });
 
+
+//////////////////////////////////////// GET MOODS ////////////////////////////////////////
 exports.getMoods = onRequest(async (req, res) => {
   // Log the start of the function
   logger.info("getMoods function triggered", { structuredData: true });
@@ -94,5 +98,82 @@ exports.getMoods = onRequest(async (req, res) => {
   } catch (e) {
     logger.error("Error getting moods", e);
     res.status(500).send({ error: "Error getting moods" });
+  }
+});
+
+
+//////////////////////////////////////// ADD QUOTE ////////////////////////////////////////
+// Add quote function using onRequest
+exports.addQuote = onRequest(async (req, res) => {
+  // Log the start of the function
+  logger.info("addQuote function triggered", { structuredData: true });
+
+  // Get the quote data from the request body
+  const quote = req.body.quote;
+  const author = req.body.author;
+  const userID = req.body.userID;
+
+  // Log the request and the quote
+  logger.info("Request body", { structuredData: true });
+  logger.info(req.body, { structuredData: true });
+
+  // - Validate the data before formatting -
+  logger.info("Validating quote data", { structuredData: true });
+
+  // Check to see if any of the fields are missing
+  if (!quote || !author || !userID) {
+    res.status(400).send({ error: 'Missing quote data' });
+    return;
+  }
+
+  // Create the format for the quote data
+  logger.info("Creating quote data", { structuredData: true });
+  const quoteData = {
+    quote: quote,
+    author: author,
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    userID: userID,
+  };
+
+  // Add the quote data to the Firestore database
+  logger.info("Adding quote data to Firestore", { structuredData: true });
+  try {
+    const docRef = db.collection("quotes").doc();
+    await docRef.set(quoteData);
+    logger.info("Document written with ID:", { id: docRef.id });
+    res.status(200).send({ message: "Quote added successfully" });
+  } catch (e) {
+    logger.error("Error adding quote", e);
+    res.status(500).send({ error: "Error adding quote" });
+  }
+});
+
+
+//////////////////////////////////////// GET MOODS ////////////////////////////////////////
+exports.getQuotes = onRequest(async (req, res) => {
+  // Log the start of the function
+  logger.info("getQuotes function triggered", { structuredData: true });
+
+  // Get the userID from the request body
+  const userID = req.query.userID;
+
+  if (!userID) {
+    res.status(400).send({ error: 'Missing userID parameter' });
+    return;
+  }
+
+  // Retrieve the quotes from the Firestore database
+  try {
+    const quotes = [];
+    const querySnapshot = await db.collection("quotes").where("userID", "==", userID).get();
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      data.timestamp = data.timestamp.toDate();
+      quotes.push(data);
+    });
+    res.status(200).send(quotes);
+  } catch (e) {
+    logger.error("Error getting quotes", e);
+    res.status(500).send({ error: "Error getting quotes" });
   }
 });
